76afd81a181786d14618acfb862b6868230b88e1
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2024-12-17 09:33:17 -0500

    fix foodbooking link

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ import type { CollectionKey } from "astro:content";
 export const twitter = "@casa_realmexicanarestaurant";
 
 export const orderUrl =
-  "https://www.foodbooking.com/ordering/restaurant/menu/info?company_uid=569b76f7-a0ca-4cd4-9c64-bef849a55ca5&restaurant_uid=976a64b7-2aca-495d-8a15-c55a29818389";
+  "https://www.foodbooking.com/ordering/restaurant/menu?company_uid=1e49066a-c69e-4685-b7da-e8cf3be95fd5&restaurant_uid=ff1b9f8b-bd36-450c-8e03-c92a037728a0";
 
 export const defaultMeta = {
   title: "Casa Azteca Mexican Restaurant",
